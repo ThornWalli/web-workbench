df650edae22be639b021a246411fb0fc854e3b87
Thorn Walli <thorn.walli@grabarzundpartner.de>
2020-05-02 01:09:07 +0200

    chore(lighthouse): remove lighthouse tests with ssr

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <component :is="coreComponent" v-if="ready" class="core" :core="core" />
+    <wb-env-error v-if="error" v-bind="error" @close="onClickError" />
   </div>
 </template>
 
 <script>
 
+import WbEnvError from '@/components/environments/Error';
+
 export default {
+  components: {
+    WbEnvError
+  },
   data () {
     return {
       ready: false,
@@ -15,10 +21,35 @@ export default {
       error: null
     };
   },
+  created () {
+    if (this.isLighthouse()) {
+      this.error = {
+        input: 'No interaction available.',
+        text: 'Not made for Lighthouse ;)',
+        stack: null,
+        code: `#${Math.floor(Math.random() * 99999999)}.${Math.floor(Math.random() * 99999999)}`
+      };
+    } else if (this.isFirefox()) {
+      this.error = {
+        input: 'No interaction available.',
+        text: 'Not made for Firefox, use a Webkit browser (e.g. Chrome).',
+        stack: null,
+        code: `#${Math.floor(Math.random() * 99999999)}.${Math.floor(Math.random() * 99999999)}`
+      };
+    }
+  },
   mounted () {
-    this.setup();
+    if (!this.error) {
+      this.setup();
+    }
   },
   methods: {
+    isFirefox () {
+      return global.navigator.userAgent.includes('Firefox/');
+    },
+    isLighthouse () {
+      return new RegExp('(Speed Insights)|(Chrome-Lighthouse)').test(window.navigator.userAgent);
+    },
     async setup () {
       this.coreComponent = await import('@/components/environments/Core').then(module => module.default);
       this.core = await import('@/web-workbench').then(module => module.default);
