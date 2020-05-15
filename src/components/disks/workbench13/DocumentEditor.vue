dd309c544564e5c1a6dc5b5ff9d22fd446de226d
Thorn Walli <lammpee@gmail.com>
2020-05-15 19:48:42 +0200

    chore(workflow): update workflow

---
+++ src/components/disks/workbench13/DocumentEditor.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="wb-disks-workbench13-document-editor">
+    <atom-input-text name="content" :options="inputTextOptions" :model="model.value" @refresh="onRefreshInputText" />
+  </div>
+</template>
+
+<script>
+
+import ContextMenuItems from '../../../web-workbench/classes/ContextMenuItems';
+import { CONFIG_NAMES } from '@/web-workbench/disks/workbench13';
+import AtomInputText from '@/components/environments/atoms/InputText';
+
+import MixinWindowComponent from '@/components/mixins/WindowComponent';
+import contextMenu from '@/web-workbench/disks/workbench13/documentEditor/contextMenu';
+
+export default {
+  components: {
+    AtomInputText
+  },
+  mixins: [
+    MixinWindowComponent
+  ],
+  props: {
+    model: {
+      type: Object,
+      default () {
+        return {
+          fsItem: null,
+          value: {
+            type: 'markdown',
+            content: ''
+          }
+        };
+      }
+    },
+    core: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      // windowsModule: this.core.modules.windows
+    };
+  },
+
+  computed: {
+    contextMenu () {
+      return new ContextMenuItems(contextMenu, { core: this.core, model: this.model });
+    },
+    inputTextOptions () {
+      return {
+        focused: this.parentFocused
+      };
+    },
+    showPreview () {
+      return this.core.config.observable[CONFIG_NAMES.DOCUMENT_EDITOR_SHOW_PREVIEW];
+    }
+  },
+  watch: {
+    // parentFocused (value) {
+    //   if (value) {
+    //     this.windowsModule.setActiveContextMenu(contextMenu({ core: this.core, model: this.model }));
+    //   } else {
+    //     this.windowsModule.setActiveContextMenu(null);
+    //   }
+    // },
+    showPreview (value) {
+      this.model.actions.togglePreview(value);
+    }
+  },
+
+  mounted () {
+    if (this.showPreview) {
+      this.model.actions.togglePreview();
+    }
+  },
+
+  // destroyed () {
+  //   if (this.parentFocused) {
+  //     this.windowsModule.setActiveContextMenu(null);
+  //   }
+  // },
+  methods: {
+    onRefreshInputText () {
+      this.$emit('refresh', { scroll: true });
+    }
+  }
+};
+</script>
+
+<style lang="postcss">
+.wb-disks-workbench13-document-editor {
+  padding: var(--default-element-margin);
+}
+</style>
---
+++ src/components/disks/workbench13/documentEditor/Info.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wb-disks-workbench13-document-editor-info">
+    <atom-markdown :content="content" />
+  </div>
+</template>
+
+<script>
+
+import ContextMenuItems from '../../../../web-workbench/classes/ContextMenuItems';
+import AtomMarkdown from '@/components/environments/atoms/Markdown';
+
+import MixinWindowComponent from '@/components/mixins/WindowComponent';
+import contextMenu from '@/web-workbench/disks/workbench13/documentEditor/contextMenu';
+
+export default {
+  components: {
+    AtomMarkdown
+  },
+  mixins: [
+    MixinWindowComponent
+  ],
+
+  props: {
+    model: {
+      type: Object,
+      default () {
+        return {
+          value: ''
+        };
+      }
+    }
+  },
+
+  data () {
+    return {
+      content: [
+        '# Document Editor', 'Version: **1.0**  \nCreated by **Thorn-Welf Walli**'
+      ].join('\n')
+    };
+  },
+  computed: {
+    contextMenu () {
+      return new ContextMenuItems(contextMenu, { core: this.core, model: this.model });
+    }
+  }
+};
+
+</script>
+
+<style lang="postcss">
+.wb-disks-workbench13-document-editor-info {
+  min-width: 320px;
+  padding: calc(var(--default-element-margin) * 2);
+}
+</style>
