35604b9cac62296f2d24c6ece718963b39cfe755
ThornWalli <ThornWalli@users.noreply.github.com>
2020-05-15 09:49:30 +0000

    deploy: 35293374e1f573f45c815c5d71201a9d8ae56cf1

---
+++ _nuxt/88.02ba26ddb59e972d1b82.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[88],{672:function(e,o,t){"use strict";var n,r=(n=t(285))&&"object"==typeof n&&"default"in n?n.default:n;r.registerVersion("firebase","7.14.4","app"),e.exports=r}}]);
--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ workbox.setConfig({
 
 // Set workbox cache names
 workbox.core.setCacheNameDetails({
-  "suffix": "105477480"
+  "suffix": "105542773"
 })
 
 // Start controlling any existing clients as soon as it activates
