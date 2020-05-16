e68b8c5cb194ca9ba31d29ec422f0043d552e9d6
ThornWalli <ThornWalli@users.noreply.github.com>
2020-05-16 00:23:41 +0000

    deploy: 30d802cd7135b6c15d5361ab26fee45bb0c0594b

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ workbox.setConfig({
 
 // Set workbox cache names
 workbox.core.setCacheNameDetails({
-  "suffix": "1.0.1"
+  "suffix": "1.1.0"
 })
 
 // Start controlling any existing clients as soon as it activates
