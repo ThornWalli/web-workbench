251b1065c2b50d494db229bc0549da72c62a99da
ThornWalli <ThornWalli@users.noreply.github.com>
2020-06-10 21:43:20 +0000

    deploy: 057d8cb982b9a6e80ed89c48b461c0cb62c355b6

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw
 
 // Set workbox config
 workbox.setConfig({
-  "CACHE_VERSION": "130861208",
+  "CACHE_VERSION": "131286318",
   "debug": false
 })
 
