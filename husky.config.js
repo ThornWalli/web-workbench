cedc69abf389ad933fdc87ba6fb4c2c2b7d0323c
Thorn Walli <lammpee@gmail.com>
2022-05-30 19:55:37 +0200

    ci(release): fix token 3

--- husky.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'npm lint',
-    'pre-push': 'npm lint && npx branch-name-lint .branchlintrc'
-  }
-};
