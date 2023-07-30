d8c08959fbfeacdab1a5112f091235499ceed783
ThornWalli <ThornWalli@users.noreply.github.com>
2023-07-30 11:32:05 +0000

    deploy: 615e0eeffd6d59ebc9c6ebbfc74f3a937b805599

---
+++ _nuxt/imprint.453241d7.js
@@ -0,0 +1,1 @@
+import{r,j as e,z as m,A as t,k as s}from"./entry.d9fe636d.js";import{P as n}from"./Page.e42e5d53.js";const _={__name:"imprint",setup(o){const a=r('execute "Imprint.md"');return e({title:"Imprint"}),(c,p)=>(s(),m(t(n),{"start-command":t(a)},null,8,["start-command"]))}};export{_ as default};
---
+++ _nuxt/index.0a13026c.js
@@ -0,0 +1,1 @@
+import{P as e}from"./Page.e42e5d53.js";import{z as r,A as a,k as o}from"./entry.d9fe636d.js";const p={__name:"index",setup(t){return(s,c)=>(o(),r(a(e)))}};export{p as default};
