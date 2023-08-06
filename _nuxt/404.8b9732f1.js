452d8d58c30b04debbc166d486c8369bac328c50
ThornWalli <ThornWalli@users.noreply.github.com>
2023-08-06 13:56:26 +0000

    deploy: da011956b978e3d33456be0d268c6464044e4ddd

---
+++ _nuxt/404.8b9732f1.js
@@ -0,0 +1,1 @@
+import{j as e,z as a,A as r,k as o}from"./entry.65cd32d6.js";import{P as s}from"./Page.9436c1ab.js";const u={__name:"404",setup(t){return e({title:"Debug"}),(c,n)=>(o(),a(r(s),{"force-no-disk":""}))}};export{u as default};
---
+++ _nuxt/debug.d6566aea.js
@@ -0,0 +1,1 @@
+import{r as t,j as r,z as s,A as e,k as o}from"./entry.65cd32d6.js";import{P as c}from"./Page.9436c1ab.js";const d={__name:"debug",setup(m){const a=t(['execute "DF0:DocumentReader.app"']);return r({title:"Debug"}),(n,u)=>(o(),s(e(c),{"start-command":e(a)},null,8,["start-command"]))}};export{d as default};
