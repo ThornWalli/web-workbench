5c386d1b9e95cc3eba68dca04d2f91e39b1c75f2
Thorn Walli <lammpee@gmail.com>
2024-08-24 19:08:20 +0200

    chore(lint): removed warn

--- packages/disks/extras13/tools/base64Converter/components/Base64Converter.vue
+++ packages/disks/extras13/tools/base64Converter/components/Base64Converter.vue
@@ -205,7 +205,7 @@ function dataURLtoBlob(dataurl) {
     n = bstr.length,
     u8arr = new Uint8Array(n);
   while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
+    u8arr[Number(n)] = bstr.charCodeAt(n);
   }
   return new Blob([u8arr], { type: mime });
 }
