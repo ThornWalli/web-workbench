d0304b20011c1dc89a47827b5a729081de491288
Thorn Walli <lammpee@gmail.com>
2025-06-02 17:51:10 +0200

    fix(extras13): update web-painting

--- packages/disk-extras13/webPainting/lib/classes/DisplayActions.ts
+++ packages/disk-extras13/webPainting/lib/classes/DisplayActions.ts
@@ -1,9 +1,23 @@
 import { ipoint, type IPoint } from '@js-basics/vector';
 import type Display from './Display';
 import { WORKER_ACTION_TYPE } from '../../types/worker';
+import { TOOLS, type ToolSelect } from '../../types/select';
+import type { DomEvents } from '@web-workbench/core/services/domEvents';
 
 export default class DisplayActions {
   constructor(private display: Display) {}
+
+  useTool(position: IPoint & number, { domEvents }: { domEvents: DomEvents }) {
+    const tool: ToolSelect = this.display.app.options.select.tool;
+    switch (tool.value) {
+      case TOOLS.ZOOM: {
+        const zoomStep = this.display.app.options.zoomStep;
+        const zoomLevel =
+          1 + (domEvents.shiftLeftActive ? -zoomStep : zoomStep);
+        this.setZoom(position, zoomLevel);
+      }
+    }
+  }
 
   setPosition(position: IPoint & number) {
     this.display.options.position = position;
@@ -14,7 +28,6 @@ export default class DisplayActions {
       }
     });
   }
-
   setZoom(position: IPoint = ipoint(0, 0), zoomLevel: number) {
     this.display.options.zoomLevel = zoomLevel;
     return this.display.action({
