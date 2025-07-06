a55645565fa1cd189eb7de922c13391563674823
Thorn Walli <lammpee@gmail.com>
2025-07-06 20:22:15 +0200

    fix(tool): adjust target position calculation for continuous and dotted freehand tools

--- packages/disk-web-paint/webPaint/workers/main/actions/client/useTool/dottedFreehand.ts
+++ packages/disk-web-paint/webPaint/workers/main/actions/client/useTool/dottedFreehand.ts
@@ -1,15 +1,21 @@
 import type { Context, UseToolMeta } from '../../../../../types/main';
 import { drawBrush } from '@web-workbench/wasm/pkg/wasm';
 import * as wasm from '../../../../../utils/wasm';
+import { ipoint } from '@js-basics/vector';
 
 export default function dottedFreehand(
   context: Context,
   useToolMeta: UseToolMeta
 ) {
-  const targetPosition = context.getTargetPosition(
+  let targetPosition = context.getTargetPosition(
     useToolMeta.position,
     useToolMeta
   );
+
+  targetPosition = ipoint(() =>
+    Math.round(targetPosition - context.useOptions.brush.size / 2)
+  );
+
   const dimension = context.getDimension();
 
   drawBrush(
